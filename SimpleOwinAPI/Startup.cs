c5ea9deecce7e800d37c38d10fa13e156996dc65
James Stackhouse <insanityisnice@hotmail.com>
2014-12-17 16:59:14 -0800

    Project creation

---
+++ SimpleOwinAPI/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+[assembly: OwinStartup(typeof(SimpleOwinAPI.Startup))]
+
+namespace SimpleOwinAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(JsonLookupHandler);
+        }
+
+        public Task JsonLookupHandler(IOwinContext context)
+        {
+            //TODO: Load JSON File base on path and return file contents.
+
+            return context.Response.WriteAsync(context.Request.Path.ToString());
+        }
+    }
+}
